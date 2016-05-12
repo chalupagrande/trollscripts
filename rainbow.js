7e1e5d16a28b5052fbc4f98043b3136649e653f3
Jamie Skinner <skinner520@gmail.com>
2016-05-12 18:04:09 -0500

    ok

--- rainbow.js
+++ rainbow.js
@@ -2,7 +2,7 @@
   var body = document.body
   var i = 0
   var a = setInterval(function(){
-    body.style['background-color'] = 'rgb('i+10', 255, 255)'
+    body.style['background-color'] = 'rgb('+(i+10)+', 255, 255)'
     i += 10
   },1000)
 })
