547a29cc4eeac4f11ccd15139e693a5202f1052e
Jamie Skinner <jamie@ninthmind.com>
2016-05-17 22:21:10 -0500

    adding gokus

--- scripts/rickroll.js
+++ scripts/rickroll.js
@@ -2,13 +2,19 @@ function roll(){
   var parent = document.createElement('div')
   parent.setAttribute('class','player')
   parent.style.position = 'fixed'
-  parent.style.zIndex = 5000;
-  parent.style.right = 0;
+  parent.style.zIndex = 9001;
   parent.style.top = 0
 
-  var dragon = document.createElement('div')
-  dragon.setAttribute('id','player')
-  parent.appendChild(dragon)
+  var video = document.createElement('div')
+  video.setAttribute('id','player')
+
+  var kids = document.createElement('div')
+  kids.setAttribute('class','gokus')
+  kids.style.position = 'absolute'
+
+
+  parent.appendChild(kids)
+  parent.appendChild(video)
   document.body.appendChild(parent)
 
   var tag = document.createElement('script')
@@ -17,25 +23,80 @@ function roll(){
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
 
   var player;
+  var w = window.innerWidth;
+  var h = window.innerHeight;
   function onYouTubeIframeAPIReady(){
     player = new YT.Player('player', {
-      height: "390",
-      width: "640",
-      videoId: "OwTH5sysquk",
+      height: h,
+      width: w,
+      videoId: "4OAm2LkrrLg",
       events: {
         'onReady': onPlayerReady,
         'onStateChange': onPlayerStateChange
+      },
+      playerVars: {
+        controls:0,
+        start: 18,
+        showinfo: 0,
+        // autoplay:1
       }
     })
   }
+  window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
 
 
 
   function onPlayerReady(event){
     console.log('ready')
-    event.target.playVideo()
+    // event.target.playVideo()
+    addGokus()
   }
   function onPlayerStateChange(event){
     console.log(event)
   }
+
+  var g1,g2,g3,g4,g5,g6,g7;
+  var deg=0;
+
+  function addGokus(){
+    g1 = document.createElement('img')
+    g1.setAttribute('width','300')
+    g1.style.position = "absolute"
+    g1.style.transition = 'transform 5s linear'
+
+    g2 = g1.cloneNode(false)
+    g3 = g1.cloneNode(false)
+    g4 = g1.cloneNode(false)
+    g5 = g1.cloneNode(false)
+    g6 = g1.cloneNode(false)
+
+    g1.setAttribute('src','https://media.giphy.com/media/raiL1MZeFhsoU/giphy.gif')
+
+    g2.setAttribute('src','https://media.giphy.com/media/T1YF688zHcq6Q/giphy.gif')
+    g3.setAttribute('src','http://rs1047.pbsrc.com/albums/b477/jujuboy614/DBZ%20Animations/goku3Danim.gif~c200')
+    g4.setAttribute('src','http://www.oocities.org/vegetto797/GokuSuperKamehameha2.gif')
+
+
+    kids.appendChild(g1)
+    kids.appendChild(g2)
+    kids.appendChild(g3)
+    kids.appendChild(g4)
+
+    var animationId = requestAnimationFrame(draw)
+
+  }
+  function draw(){
+    deg+=360
+    setTimeout(function(){
+      animateGokus(deg)
+      requestAnimationFrame(draw)
+    }, 5000)
+  }
+
+  function animateGokus(deg){
+    console.log(deg)
+    g1.style.transform = 'rotate('+deg+'deg)'
+    g2.style.transform = 'rotate('+deg+'deg)'
+  }
+
 }
