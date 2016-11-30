6566317c5873b1366f43acc23e06a35a9351672a
Jamie Skinner <jamie@ninthmind.com>
2016-11-30 13:22:11 -0600

    added draw your number

--- src/phonenumbers.js
+++ src/phonenumbers.js
@@ -105,3 +105,69 @@ function slotListeners(){
     })
   })
 }
+
+//CHECKBOX NIGHTMARE -------------
+
+let numberPatterns = [
+  ["r1c2","r1c3","r2c1","r2c4","r3c1","r3c4","r4c1","r4c4","r5c1","r5c4","r6c1","r6c4","r7c2","r7c3"], //0
+  ["r1c3","r2c2","r2c3","r3c3","r4c3","r5c3","r6c3","r7c3"],  //1
+  ["r1c2","r1c3","r2c1","r2c4","r3c4","r4c3","r5c2","r6c1","r7c1","r7c2","r7c3","r7c4"], //2
+  ["r1c2","r1c3","r2c1","r2c4","r3c4","r4c2","r4c3","r5c4","r6c1","r6c4","r7c2","r7c3"], //3
+  ["r1c4","r2c3","r2c4","r3c2","r3c4","r4c1","r4c4","r5c1","r5c2","r5c3","r5c4","r6c4","r7c4"], //4
+  ["r1c1","r1c2","r1c3","r1c4","r2c1","r3c1","r4c1","r4c2","r4c3","r5c4","r6c1","r6c4","r7c2","r7c3"], //5
+  ["r1c2","r1c3","r2c1","r2c4","r3c1","r4c1","r4c2","r4c3","r5c1","r5c4","r6c1","r6c4","r7c2","r7c3"],  //6
+  ["r1c1","r1c2","r1c3","r1c4","r2c1","r2c4","r3c4","r4c3","r5c3","r6c2","r7c2"],  //7
+  ["r1c2","r1c3","r2c1","r2c4","r3c1","r3c4","r4c2","r4c3","r5c1","r5c4","r6c1","r6c4","r7c2","r7c3"], //8
+  ["r1c2","r1c3","r2c1","r2c4","r3c1","r3c4","r4c2","r4c3","r4c4","r5c4","r6c1","r6c4","r7c2","r7c3"] //9
+]
+
+function drawNumbers(){
+  var numberGroups = [...document.querySelectorAll('.checkbox--number')]
+  numberGroups.forEach((g,i)=>{
+    let pattern = numberPatterns[i];
+    let allInputs = [...g.querySelectorAll('input[type=checkbox]')]
+    let patternIndex = 0
+    for(let k = 0; k < allInputs.length; k++){
+      if(pattern[patternIndex] == allInputs[k].getAttribute('name')){
+        allInputs[k].checked = true
+        patternIndex += 1
+      } else {
+        allInputs[k].checked = false
+      }
+    }
+  })
+  document.querySelector('.checkbox-nightmare .number').innerText = formatNumber(getNumber())
+}
+function getNumber(){
+  var result = '';
+  [...document.querySelectorAll('.checkbox--number')].forEach((digit)=>{
+    result += getCheckboxNumber(digit).toString()
+  })
+  return result
+}
+function getCheckboxNumber(group){
+  for(let k = 0; k < numberPatterns.length; k++){
+    if(checkPattern(numberPatterns[k], group)) return k
+  }
+  return '_'
+}
+
+function checkPattern(pattern, group){
+  let inputs = [...group.querySelectorAll('input[type=checkbox]')]
+  for(var i = 0; i < inputs.length; i++){
+    let id = inputs[i].getAttribute('name')
+    if(inputs[i].checked && pattern.indexOf(id) < 0) return false
+    if(!inputs[i].checked && pattern.indexOf(id) >= 0) return false
+  }
+  return true
+}
+
+function addCheckboxListeners(){
+  var result = document.querySelector('.checkbox-nightmare .number');
+  [...document.querySelectorAll('.checkbox-field input[type=checkbox]')].map((el)=>{
+    el.addEventListener('change', (e)=>{
+      result.innerText = formatNumber(getNumber())
+    })
+  })
+
+}
