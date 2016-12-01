1b78a481fad1d9977793328eb46294c1d81eba07
Jamie Skinner <jamie@ninthmind.com>
2016-12-01 17:48:21 -0600

    overflow check

--- src/phonenumbers.js
+++ src/phonenumbers.js
@@ -174,18 +174,17 @@ function addCheckboxListeners(){
 //Pi -------------
 var index = -1
 function forward(){
-  index += 1
   var piAll = document.getElementById('pi-all');
+  if(index < piAll.innerText.length) index += 1
   var number = piAll.innerText.slice(index, index+10)
   var span = `<span class="pi-selection">${number}</span>`
   piAll.innerHTML = piAll.innerText.replace(number, span)
 
 }
 function backward(){
-  index -= 1
+  if(index > 0 )index -= 1
   var piAll = document.getElementById('pi-all');
   var number = piAll.innerText.slice(index, index+10)
   var span = `<span class="pi-selection">${number}</span>`
   piAll.innerHTML = piAll.innerText.replace(number, span)
-
 }
